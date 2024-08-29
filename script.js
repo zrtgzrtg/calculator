3f9e71cd5e14fdb630040b6486ed7cb1ebfdc4a0
zrtg <[thomas4stank@gmail.com]>
2024-08-29 19:57:16 +0200

    Finished until step 3
    created operate function. Didn't test yet

--- script.js
+++ script.js
@@ -10,5 +10,29 @@ function multiply(num1,num2) {
     return num1 * num2
 }
 function divide(num1,num2) {
+    if(num2 === 0) {
+        throw Error("Cannot divide by 0")
+    }
     return num1 / num2
+}
+
+let firstNumber = 0
+let secondNumber = 0
+let operator = ""
+
+function operate(num1,operator,num2) {
+    const legalOperators = ["+","-","*","/"]
+    if(!operator in legalOperators) {
+        throw Error("Illegal Operation chosen: " + operator)
+    }
+    switch(operator) {
+        case "+":
+            return add(num1,num2)
+        case "-":
+            return substract(num1,num2)
+        case "*":
+            return multiply(num1,num2)
+        case "/":
+            return divide(num1,num2)
+    }
 }
